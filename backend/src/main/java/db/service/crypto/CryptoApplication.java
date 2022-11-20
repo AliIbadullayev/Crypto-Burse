925ac4bef6384b7d599f41dae625cd77289c84c0
Ibadullaev_Ali <ali.ibadullayev.02@gmail.com>
2022-11-20 22:57:43 +0300

    configure backend structure

--- backend/src/main/java/db/service/crypto/CryptoApplication.java
+++ backend/src/main/java/db/service/crypto/CryptoApplication.java
@@ -2,6 +2,10 @@ package db.service.crypto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class CryptoApplication {
@@ -10,4 +14,14 @@ public class CryptoApplication {
         SpringApplication.run(CryptoApplication.class, args);
     }
 
+    @Configuration
+    @EnableWebSecurity
+    public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity security) throws Exception
+        {
+            security.httpBasic().disable();
+        }
+    }
 }
