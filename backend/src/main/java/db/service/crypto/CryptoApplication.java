ec2eb89239169085b63d1b40dabc8ac21d0ba0dc
Ibadullaev_Ali <ali.ibadullayev.02@gmail.com>
2022-11-20 22:18:26 +0300

    Initial backend commit

---
+++ backend/src/main/java/db/service/crypto/CryptoApplication.java
@@ -0,0 +1,13 @@
+package db.service.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/db/service/crypto/CryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package db.service.crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
