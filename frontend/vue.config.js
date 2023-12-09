7fde14680c524f2deea0b955ac48a55ec242c60a
va.v.osipov <va.v.osipov@tinkoff.ru>
2023-12-09 16:39:10 +0300

    Configuration setup for local development

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 8080,
+    port: 7766,
     proxy: {
       '/api/v1': {
-        target: 'http://localhost:8081/',
+        target: 'http://localhost:8091/',
         changeOrigin: true
       },
     }
